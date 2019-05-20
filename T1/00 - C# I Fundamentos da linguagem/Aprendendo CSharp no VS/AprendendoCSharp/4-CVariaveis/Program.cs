6b91de1fd84fba77744f04b2afa8b33381d74ee4
stag003 <stag003@benner.com.br>
2019-05-20 10:09:38 -0300

    Criando diferentes variaveis double,int,float,short,long

---
+++ T1/00 - C# I Fundamentos da linguagem/Aprendendo CSharp no VS/AprendendoCSharp/4-CVariaveis/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_CVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 4");
+            double salario = 1200.50;
+
+            int salarioEmInteiro = (int)salario;
+
+            Console.WriteLine(salarioEmInteiro);
+
+            long idade = 130000000000000;
+            Console.WriteLine(idade);
+
+            short quantidadedeProdutos = 15000;
+            Console.WriteLine(quantidadedeProdutos);
+
+            float altura = 1.80f;
+            Console.WriteLine(altura);
+
+            Console.ReadLine();
+        }
+    }
+}
