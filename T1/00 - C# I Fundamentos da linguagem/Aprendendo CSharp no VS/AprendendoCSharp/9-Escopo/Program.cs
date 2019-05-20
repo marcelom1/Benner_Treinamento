dd402a961c55b87f7122c79dfb59a3367dcb3830
Marcelo Miglioli <marcelo.miglioli@hotmail.com>
2019-05-20 13:24:32 -0300

    Finalizando projeto 9 - Escopo

--- T1/00 - C# I Fundamentos da linguagem/Aprendendo CSharp no VS/AprendendoCSharp/9-Escopo/Program.cs
+++ T1/00 - C# I Fundamentos da linguagem/Aprendendo CSharp no VS/AprendendoCSharp/9-Escopo/Program.cs
@@ -10,6 +10,25 @@ namespace _9_Escopo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Executando projeto 9 - Escopo");
+
+            int idadeJoao = 18;
+            bool acompanhado = true;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+                mensagemAdicional = "João está acompanhado!";
+            else
+                mensagemAdicional = "João NÃO está acompanhado";
+
+            if (idadeJoao >= 18 && acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar.");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+                Console.WriteLine("Não pode entrar");
+            Console.ReadLine();
         }
     }
 }
