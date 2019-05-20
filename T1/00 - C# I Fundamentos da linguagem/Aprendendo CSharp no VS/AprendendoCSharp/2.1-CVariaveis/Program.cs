7cb4e9e8500e1d140fa205fb84d62e49c96a9d07
stag003 <stag003@benner.com.br>
2019-05-20 09:26:10 -0300

    Praticando criação de variaveis do tipo int

---
+++ T1/00 - C# I Fundamentos da linguagem/Aprendendo CSharp no VS/AprendendoCSharp/2.1-CVariaveis/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1_CVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá novo teste");
+            int idade = 37;
+            Console.WriteLine("A Idade é " + idade + ", parabéns!");
+            Console.ReadLine();
+        }
+    }
+}
