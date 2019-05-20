46da80b01970d108e5ea5eb95648bf525eeb23d5
Marcelo Miglioli <marcelo.miglioli@hotmail.com>
2019-05-20 17:05:08 -0300

    Desafio opcional: Fatorial

---
+++ T1/00 - C# I Fundamentos da linguagem/Aprendendo CSharp no VS/AprendendoCSharp/DOFatorial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DOFatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fatorial = 1;
+            for (int i = 1; i <= 10; i++)
+            {
+                fatorial *= i;
+            }
+            Console.WriteLine("Fatorial de 10 é: " + fatorial);
+            Console.ReadLine();
+        }
+    }
+}
