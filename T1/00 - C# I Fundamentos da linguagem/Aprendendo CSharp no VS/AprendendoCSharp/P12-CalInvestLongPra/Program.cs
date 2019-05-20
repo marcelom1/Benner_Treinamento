7f7621c4c0b880765bf0406c7964a2f33b584d63
Marcelo Miglioli <marcelo.miglioli@hotmail.com>
2019-05-20 16:12:41 -0300

    For encadeado

---
+++ T1/00 - C# I Fundamentos da linguagem/Aprendendo CSharp no VS/AprendendoCSharp/P12-CalInvestLongPra/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P12_CalInvestLongPra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 12");
+            double valorInvestido = 1000.00;
+            double fatorRendimento = 1.0036;
+
+            for (int cont1 = 1; cont1 <= 5; cont1++)
+            {
+                for (int cont2 = 1; cont2 <= 12; cont2++)
+                {
+                    valorInvestido *= fatorRendimento;
+               
+
+                }
+                fatorRendimento += 0.0010;
+            }
+            Console.WriteLine("Ao término do investimento, você terá R$" + valorInvestido);
+            Console.ReadLine();
+        }
+    }
+}
