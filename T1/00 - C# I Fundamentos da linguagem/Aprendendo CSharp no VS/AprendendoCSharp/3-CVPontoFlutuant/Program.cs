ca0bf201c0b63e617fc1da956c2c0bd5d9428ff0
stag003 <stag003@benner.com.br>
2019-05-20 09:42:55 -0300

    Criando Variaveis do tipo double

---
+++ T1/00 - C# I Fundamentos da linguagem/Aprendendo CSharp no VS/AprendendoCSharp/3-CVPontoFlutuant/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_CVPontoFlutuant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto  3 Criando variáveis ponto flutuante");
+
+            double salario;
+
+            salario = 1220.70;
+            Console.WriteLine(salario);
+
+            double idade;
+            idade = 15.0 / 2;
+
+            Console.WriteLine("15.0 / 2 = " + idade);
+
+            idade = 15 / 2;
+
+            Console.WriteLine("15 / 2 = " + idade);
+
+            Console.WriteLine("Termino da aplicação...");
+            Console.ReadLine();
+        }
+    }
+}
