2751abd6c9601367ac29de86d0e45b2769159cd0
stag003 <stag003@benner.com.br>
2019-05-20 10:17:26 -0300

    Testando algumas conversões em C#

---
+++ T1/00 - C# I Fundamentos da linguagem/Aprendendo CSharp no VS/AprendendoCSharp/4.1CVariaveis/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._1CVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float pontoflutuante = 3.14f;
+
+            double salario = 1270.50;
+            int valor = (int)salario;
+            Console.WriteLine(valor);
+
+            double valor1 = 0.2;
+            double valor2 = 0.1;
+            double total = valor1 + valor2;
+
+            Console.WriteLine(total);
+
+            Console.ReadLine();
+        }
+    }
+}
