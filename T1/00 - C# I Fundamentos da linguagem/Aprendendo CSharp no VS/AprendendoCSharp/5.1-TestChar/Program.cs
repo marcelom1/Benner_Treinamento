be5dee72942b8c8c33263b31e1d0b96cf50aafbb
stag003 <stag003@benner.com.br>
2019-05-20 10:49:44 -0300

    Testando a manipulação de char e string

---
+++ T1/00 - C# I Fundamentos da linguagem/Aprendendo CSharp no VS/AprendendoCSharp/5.1-TestChar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._1_TestChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letra = 'a';
+            Console.WriteLine(letra);
+
+            char valor = (char)65;
+            Console.WriteLine(valor);
+
+            valor = (char)(valor + 1);
+            Console.WriteLine(valor);
+
+            string palavra = "alura cursos online de tecnologia";
+            Console.WriteLine(palavra);
+
+            palavra = palavra + 2020;
+            Console.WriteLine(palavra);
+            Console.ReadLine();
+        }
+    }
+}
