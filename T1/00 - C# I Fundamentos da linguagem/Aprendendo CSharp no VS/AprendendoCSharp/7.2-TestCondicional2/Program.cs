50ad2362820faa6dc469a66e30d3d18dc55f6133
Marcelo Miglioli <marcelo.miglioli@hotmail.com>
2019-05-20 13:34:46 -0300

    Um pouco mais de IF - Treinando condições

---
+++ T1/00 - C# I Fundamentos da linguagem/Aprendendo CSharp no VS/AprendendoCSharp/7.2-TestCondicional2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _7._2_TestCondicional2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando condicionais");
+
+            int idade = 16;
+            int quantidadePessoas = 3;
+            bool acompanhado = quantidadePessoas>=2;
+            if (idade>=18 && acompanhado)
+            {
+                Console.WriteLine("Seja bem Vindo");
+            }
+            else
+            {
+                Console.WriteLine("Infelizmente você não pode entrar");
+            }
+        }
+    }
+}
