019a2d09ca58b3e6c31a11eb10ad8de078019eae
stag003 <stag003@benner.com.br>
2019-05-20 09:48:55 -0300

    Teste com ponto flutuante

---
+++ T1/00 - C# I Fundamentos da linguagem/Aprendendo CSharp no VS/AprendendoCSharp/3.1-PontoFlutuant/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._1_PontoFlutuant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 1250.70;
+            Console.WriteLine("Meu salario é " + salario);
+
+            double divisao = 5.0 / 2;
+            Console.WriteLine(divisao);
+
+
+            Console.WriteLine("Fim da aplicação");
+            Console.ReadLine();
+        }
+    }
+}
