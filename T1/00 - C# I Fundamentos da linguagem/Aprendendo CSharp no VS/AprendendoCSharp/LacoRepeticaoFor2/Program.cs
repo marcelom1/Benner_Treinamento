c49f07131d140cfcdf01382e203d6b65bd1965f8
Marcelo Miglioli <marcelo.miglioli@hotmail.com>
2019-05-20 16:50:03 -0300

    imprimindo uma matriz triangular com for encadeado

---
+++ T1/00 - C# I Fundamentos da linguagem/Aprendendo CSharp no VS/AprendendoCSharp/LacoRepeticaoFor2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LacoRepeticaoFor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //imprimindo uma matriz triangular
+
+            for (int linha = 0; linha < 10; linha++)
+            {
+                for (int coluna = 0; coluna <=linha; coluna++)
+                {
+                   
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
