8cd84e3f236e5e4a72d077d4707fdb11a4ca0be9
Marcelo Miglioli <marcelo.miglioli@hotmail.com>
2019-05-21 16:08:39 -0300

    Utilizando Construtores na class ContaCorrente

---
+++ T1/01 - C# II Introdução OO/ByteBank/07-ByteBank/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(333, 12345);
+            
+        
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
