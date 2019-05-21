9db812ac78f54ba606f8cf885a3bfcbe49e12ef3
Marcelo Miglioli <marcelo.miglioli@hotmail.com>
2019-05-21 13:52:54 -0300

    Mão na massa: Referências

---
+++ T1/01 - C# II Introdução OO/ByteBank/Exe.02-ByteBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exe._02_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            conta.titular = new Cliente();
+
+            conta.titular.nome = "Gabriele";
+
+            Console.WriteLine(conta.titular.nome);
+
+            Console.ReadLine();
+        }
+    }
+}
