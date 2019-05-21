b1815e00977652e5489c15b38bc5f78a2bcc570f
stag003 <stag003@benner.com.br>
2019-05-21 10:37:41 -0300

    Metodo Sacar implementado na class ContaCorrente

---
+++ T1/01 - C# II Introdução OO/ByteBank/04-ByteBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaBruno = new ContaCorrente();
+            contaBruno.titular = "Bruno";
+
+            Console.WriteLine(contaBruno.saldo);
+
+            bool resultado = contaBruno.Sacar(50);
+            Console.WriteLine(contaBruno.saldo);
+            Console.WriteLine(resultado);
+
+            Console.ReadLine();
+        }
+    }
+}
