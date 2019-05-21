d2c6987601a6796c9970f7d4d86e02f42f922538
stag003 <stag003@benner.com.br>
2019-05-21 09:38:08 -0300

    Atribuindo valores padrões nas class

---
+++ T1/01 - C# II Introdução OO/ByteBank/02-ByteBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = "Gabriela";
+
+            Console.WriteLine("Titual: " + conta.titular);
+            Console.WriteLine("Saldo: " + conta.saldo);
+
+            Console.ReadLine();
+
+        }
+    }
+}
