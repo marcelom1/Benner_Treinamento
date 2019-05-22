d4aa75e85aa98d41768a2b19342088aa134a76dd
stag003 <stag003@benner.com.br>
2019-05-22 11:13:40 -0300

    Mão na massa - sobrecargar na class GerenciadorBonificacao

---
+++ T1/03 - C# III Herença e Interface/ByteBank/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double  salario { get; set; }
+
+
+        public double GetBonificacao()
+        {
+            return salario * 0.10;
+        }
+    }
+}
---
+++ T1/03 - C# III Herença e Interface/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Funcionarios;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario carlos = new Funcionario();
+            carlos.Nome = "Carlos";
+            carlos.CPF = "589.541.954-71";
+            carlos.salario = 2000;
+
+            Diretor gabriela = new Diretor();
+            gabriela.Nome = "Gabriela";
+            gabriela.CPF = "965.652.852-72";
+            gabriela.salario = 5000;
+
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            gerenciador.Registrar(carlos);
+            gerenciador.Registrar(gabriela);
+
+            Console.WriteLine("Total de bonificação: " + gerenciador.GetTotalBonificacao());
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
