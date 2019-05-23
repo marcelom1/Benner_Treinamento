34f4fed7170b09df34c302e5bbc441e87e26a043
stag003 <stag003@benner.com.br>
2019-05-23 10:20:59 -0300

    Tratando algumas exceções de erros

---
+++ T1/04 - C# IV Exceções/ByteBank/ByteBank/ContaCorrente.cs
@@ -0,0 +1,100 @@
+﻿//using _05_ByteBank;
+
+namespace _07_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+        private int _agencia;
+        private int _numero;
+        private double _saldo = 100;
+        public static int TotalDeContasCriadas { get; private set; }
+        public static double TaxaOperacao { get; private set; }
+
+        public int Numero
+        {
+
+            get { return _numero; }
+            set
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
+                _numero = value;
+            }
+        }
+        public int Agencia
+        {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
+                _agencia = value;
+            }
+
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+            TotalDeContasCriadas++;
+
+            TaxaOperacao = 30 / TotalDeContasCriadas;
+        }
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                _saldo = value;
+            }
+        }
+
+
+        public bool Sacar(double valor)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this._saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+
+            this._saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
---
+++ T1/04 - C# IV Exceções/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Metodo();
+            }
+            catch (NullReferenceException erro)
+            {
+                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine("Aconteceu um erro!");
+            }
+            Console.ReadLine();
+        }
+        private static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            try
+            {
+                int resultado = Dividir(10, divisor);
+                Console.WriteLine("Resultado da divisão de 10 por " + " é " + resultado);
+            }
+            catch (DivideByZeroException erro)
+            {
+                Console.WriteLine(erro.Message);
+                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine("Não é possível fazer uma divisão por 0!");
+            }
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            ContaCorrente conta = null;
+            return numero / divisor;
+        }
+    }
+}
