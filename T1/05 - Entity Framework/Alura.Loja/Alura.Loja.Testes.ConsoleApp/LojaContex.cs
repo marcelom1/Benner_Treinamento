d03f5eac81f4cd18b94b1a6a93382c43cd838eaa
stag003 <stag003@benner.com.br>
2019-05-24 10:45:51 -0300

    Primeiro uso da Ferramenta Entity

---
+++ T1/05 - Entity Framework/Alura.Loja/Alura.Loja.Testes.ConsoleApp/LojaContex.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class LojaContex : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LojaDB;Trusted_Connection=true;");
+
+
+        }
+    }
+}
