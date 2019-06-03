937cf5028b2c0e12257d03eb7b17f4f479a19f94
stag003 <stag003@benner.com.br>
2019-06-03 11:08:16 -0300

    Acrescentando mais validações nos campos do produtos

--- T1/08 - ASPNET MVC 5/CaelumEstoque/CaelumEstoque/Models/Produto.cs
+++ T1/08 - ASPNET MVC 5/CaelumEstoque/CaelumEstoque/Models/Produto.cs
@@ -13,6 +13,7 @@ namespace CaelumEstoque.Models
         [Required, StringLength(20)]
         public String Nome { get; set; }
 
+        [Range(0.0 , 10000.00)]
         public float Preco { get; set; }
 
         public CategoriaDoProduto Categoria { get; set; }
