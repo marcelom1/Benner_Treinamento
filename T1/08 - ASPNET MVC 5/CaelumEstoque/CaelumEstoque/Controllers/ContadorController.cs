dd1ff3742010ff55a0092a05ad0c341a2788e91c
stag003 <stag003@benner.com.br>
2019-06-04 11:55:09 -0300

    Criando Controller Contador

---
+++ T1/08 - ASPNET MVC 5/CaelumEstoque/CaelumEstoque/Controllers/ContadorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class ContadorController : Controller
+    {
+        // GET: Contador
+        public ActionResult Index()
+        {
+            object valorNaSession = Session["contador"];
+            int contador = Convert.ToInt32(valorNaSession);
+            return View();
+        }
+    }
+}
