b5a36b4c6805ec20c0739310d97674b4b97ea6fa
stag003 <stag003@benner.com.br>
2019-06-19 11:57:32 -0300

    Curso TTD - Projeto Inicial

---
+++ T2/3 - TTD/Alura.LeilaoOnline/Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Alura.LeilaoOnline.Core;
+
+namespace Alura.LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano",leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano,500);
+            leilao.RecebeLance(maria, 600);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(maria, 600);
+
+            leilao.TerminaPregao();
+
+            Console.WriteLine(leilao.Ganhador.Valor);
+
+            Console.ReadLine();
+
+        }
+    }
+}
