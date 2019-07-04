b6619ca405a5920d453f2e942bd4bdffe1843b25
Marcelo Miglioli <marcelo.miglioli@hotmail.com>
2019-07-04 10:06:27 -0300

    Finalizando Curso JavaScript - Jogo da Forca

--- T3/5-ProjetoJavaScript/gallows/js/jogo.js
+++ T3/5-ProjetoJavaScript/gallows/js/jogo.js
@@ -1,21 +1,80 @@
-var criaJogo = function(){
-  
+var criaJogo = function(sprite){
+    
+    var palavraSecreta = '';
+    var lacunas = [];
+    var etapa = 1
+    
+    var ganhou = function () {
+         return lacunas.length 
+            ? !lacunas.some(function(lacuna) {
+                return lacuna == '';
+            })
+            : false;
+    };
+
+    var perdeu = function () {
+        return sprite.isFinished();
+    };
+
+    var ganhouOuPerdeu = function () {
+        return ganhou() || perdeu();
+    };
+
+    var reinicia = function () {
+        etapa = 1;
+        lacunas=[];
+        palavraSecreta="";
+        sprite.reset();
+    };
+    
+    var proccessaChute = function(chute){
+        if (!chute.trim()) throw Erro('Chute inválida!');
+        var exp = new RegExp(chute,'gi'),resultado, acertou=false;
+        while(resultado=exp.exec(palavraSecreta)){
+            lacunas[resultado.index]=chute;
+            acertou=true;
+        }
+        if (!acertou){
+            sprite.nextFrame();
+        }
+        
+    };
+    var criarLacunas = function(){
+        for(var i=0; i<palavraSecreta.length;i++){
+            lacunas.push("");
+        }
+    }
+    
+    var proximaEtapa = function(){
+      etapa = 2;  
+    };
+    
     var setPalavraSecreta = function (palavra){
+        if (!palavra.trim()) throw Erro('palavra Secreta inválida!');
+        palavraSecreta = palavra;
+        criarLacunas();
+        proximaEtapa();
         
     };
     
     var getLacunas = function(){
-        
+        return lacunas;
     };
     
     var getEtapa = function(){
-        
+        return etapa;
     };
     
     return{ 
         setPalavraSecreta: setPalavraSecreta,
         getLacunas: getLacunas,
-        getEtapa: getEtapa
+        getEtapa: getEtapa,
+        proccessaChute: proccessaChute,
+        ganhou: ganhou,
+        perdeu: perdeu,
+        ganhouOuPerdeu: ganhouOuPerdeu,
+        reinicia: reinicia
+        
     };
     
 };
